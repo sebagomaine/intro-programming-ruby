08920b5fd6614a80aef4e208038efea40a14e880
Andy Holmes <beawareherenow@gmail.com>
2022-02-17 13:02:25 -0500

    initial

---
+++ basics/ex_3.rb
@@ -0,0 +1,9 @@
+movies = {
+  :starwars => 1977,
+  :theempirestrikesback => 1980,
+  :returnofthejedi => 1983
+}
+
+puts movies[:starwars]
+puts movies[:theempirestrikesback]
+puts movies[:returnofthejedi]
