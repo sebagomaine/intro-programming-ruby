c265f3b9a929b0aaa71657f1cdc7c8883d9b321b
Andy Holmes <beawareherenow@gmail.com>
2022-02-17 13:22:34 -0500

    initial

---
+++ basics/ex_7.rb
@@ -0,0 +1,2 @@
+# Ruby encountered a closing parentheses ) but it was expecting a closing bracket }
+# Or, put another way, there is an opening bracket { in the code that does not have a closing bracket }.
