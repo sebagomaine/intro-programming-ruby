900b03d3ae5cb8bdac070edaf88e5b9c38db9085
Andy Holmes <beawareherenow@gmail.com>
2022-02-17 13:14:20 -0500

    initial

---
+++ basics/ex_5.rb
@@ -0,0 +1,8 @@
+five_factorial = 5 * 4 * 3 * 2
+six_factorial = 6 * five_factorial
+seven_factorial = 7 * six_factorial
+eight_factorial = 8 * seven_factorial
+puts five_factorial
+puts six_factorial
+puts seven_factorial
+puts eight_factorial
