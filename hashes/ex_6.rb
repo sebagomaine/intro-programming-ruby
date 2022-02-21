5d142c6df3b55810a635822f355def29098144a0
Andy Holmes <beawareherenow@gmail.com>
2022-02-21 18:51:22 -0500

    initial

---
+++ hashes/ex_6.rb
@@ -0,0 +1,9 @@
+x = "hi there"
+
+# key is the symbol x :x
+my_hash = {x: "some value"}
+puts my_hash[:x]
+
+# key is the string 'hi there'
+my_hash2 = {x => "some value"}
+puts my_hash2['hi there']
