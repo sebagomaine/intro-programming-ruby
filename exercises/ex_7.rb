409f93d0edb6a8aadd972cb553c34d439a1425af
Andy Holmes <beawareherenow@gmail.com>
2022-02-22 12:30:09 -0500

    initial

---
+++ exercises/ex_7.rb
@@ -0,0 +1,11 @@
+# An array is an ordered collection whose elements
+# are associated (and accessed) by ordered indexes
+# beginning with zero (0). The array indexes are
+# constructs of the language and not user defined.
+#
+# A hash is also a collection, but its values are
+# associated with user defined keys that although
+# not required, are most commonly symbols that
+# usually have some apparent descriptive relationship
+# to the hash value. This relationship is called a
+# key value pair.
